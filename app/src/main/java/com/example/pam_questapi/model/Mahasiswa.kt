53698291cb73cbf5cfb204d19a3b943ba434d00e
kyy08 <kyyelzz1@gmail.com>
2024-12-30 06:52:40 +0700

    membuat data Mahasiswa

---
+++ app/src/main/java/com/example/pam_questapi/model/Mahasiswa.kt
@@ -0,0 +1,16 @@
+package com.example.pam_questapi.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Mahasiswa(
+    val nim : String,
+    val nama: String,
+    val alamat: String,
+
+    @SerialName("jenis_kelamin")
+    val jenisKelamin: String,
+    val kelas : String,
+    val angkatan: String
+)
