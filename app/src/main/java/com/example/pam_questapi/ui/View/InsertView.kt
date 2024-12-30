e1f00d530d18a42125633ef0db52af794f7c7773
kyy08 <kyyelzz1@gmail.com>
2024-12-30 09:09:20 +0700

    membuat DestinasiEntry : DestinasiNavigasi di InsertView

---
+++ app/src/main/java/com/example/pam_questapi/ui/View/InsertView.kt
@@ -0,0 +1,8 @@
+package com.example.pam_questapi.ui.View
+
+import com.example.pam_questapi.ui.navigation.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Entry Mhs"
+}
