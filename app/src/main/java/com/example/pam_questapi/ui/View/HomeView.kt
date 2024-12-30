bc68bc4cfe99a809af5b579ee3cc54b5fe0ef976
kyy08 <kyyelzz1@gmail.com>
2024-12-30 10:51:45 +0700

    membuat object DestinasiHome : DestinasiNavigasi

---
+++ app/src/main/java/com/example/pam_questapi/ui/View/HomeView.kt
@@ -0,0 +1,9 @@
+package com.example.pam_questapi.ui.View
+
+import com.example.pam_questapi.ui.navigation.DestinasiNavigasi
+
+// Objek untuk mendefinisikan rute dan judul layar home
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home" // Rute navigasi layar home
+    override val titleRes = "Home Mhs"
+}
