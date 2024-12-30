7b3ef02da7998d45cc5fdd8a80244bfd9bb46953
kyy08 <kyyelzz1@gmail.com>
2024-12-30 07:15:17 +0700

    membuat Mahasiswa Service

---
+++ app/src/main/java/com/example/pam_questapi/service/MahasiswaService.kt
@@ -0,0 +1,33 @@
+package com.example.pam_questapi.service
+
+import com.example.pam_questapi.model.Mahasiswa
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
+
+
+interface MahasiswaService {
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json",
+    )
+    @GET("bacamahasiswa.php")
+    suspend fun getMahasiswa(): List<Mahasiswa>
+
+    @GET("bacamahasiswa.php/{nim}")
+    suspend fun getMahasiswaById(@Query("nim") nim: String): Mahasiswa
+
+    @POST("insertmahasiswa.php")
+    suspend fun insertMahasiswa(@Body mahasiswa: Mahasiswa)
+
+    @PUT("editmahasiswa.php/{nim}")
+    suspend fun updateMahasiswa(@Query("nim")nim: String, @Body mahasiswa: Mahasiswa)
+
+    @DELETE("deletemahasiswa.php/{nim}")
+    suspend fun deleteMahasiswa(@Query("nim")nim: String): Response<Void>
+}
