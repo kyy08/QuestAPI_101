5fc234fa12d0fbcc2241e2bd3f7ab2d9a55abebb
kyy08 <kyyelzz1@gmail.com>
2024-12-30 07:07:39 +0700

    membuat Mahasiswa Repository

---
+++ app/src/main/java/com/example/pam_questapi/repository/MahasiswaRepository.kt
@@ -0,0 +1,45 @@
+package com.example.pam_questapi.repository
+
+import com.example.pam_questapi.model.Mahasiswa
+import com.example.pam_questapi.service.MahasiswaService
+import okio.IOException
+
+interface MahasiswaRepository{
+    suspend fun getMahasiswa(): List<Mahasiswa>
+    suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+    suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa)
+    suspend fun deleteMahasiswa(nim: String)
+    suspend fun getMahasiswaById(nim: String): Mahasiswa
+}
+
+class NetworkMahasiswaRepository(
+    private val mahasiswaApiService: MahasiswaService
+) : MahasiswaRepository{
+    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
+        mahasiswaApiService.insertMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
+        mahasiswaApiService.updateMahasiswa(nim,mahasiswa)
+    }
+
+    override suspend fun deleteMahasiswa(nim: String) {
+        try {
+            val response = mahasiswaApiService.deleteMahasiswa(nim)
+            if (!response.isSuccessful){
+                throw IOException("Failed to delete mahasiswa. HTTP Status code: ${response.code()}")
+            }else{
+                response.message()
+                println(response.message())
+            }
+        }catch (e:Exception){
+            throw e
+        }
+    }
+
+    override suspend fun getMahasiswa(): List<Mahasiswa> = mahasiswaApiService.getMahasiswa()
+    override suspend fun getMahasiswaById(nim: String): Mahasiswa {
+        return mahasiswaApiService.getMahasiswaById("nim")
+    }
+
+}
